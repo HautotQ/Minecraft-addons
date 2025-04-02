ddb731cfc2602cff7f97dc8917f6d6c4cfedfdb3
Tabarcraft <166629849+HautotQ@users.noreply.github.com>
2025-04-02 14:45:28 +0200

    Create main.js

---
+++ Scripts/main.js
@@ -0,0 +1,10 @@
+import { world, system } from "@minecraft/server";
+
+function mainTick() {
+  if (system.currentTick === 400) {
+    world.sendMessage("All systems GO!");
+  }
+  system.run(mainTick);
+}
+
+system.run(mainTick);
